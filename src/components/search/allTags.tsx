da816d2e9648ae887967220e016cb19ae619a4fb
yauheni <yauheni.ortsiukh@modsen-software.com>
2023-02-24 18:42:02 +0300

    fix header

--- src/components/search/allTags.tsx
+++ src/components/search/allTags.tsx
@@ -7,19 +7,21 @@ type TagsProps = {
 };
 
 const AllTags = ({ tags }: TagsProps) => {
-  console.log(tags);
-  
   return (
     <>
-      {tags.length > 0 && <div className={styles.tags_title}>All tags</div>}
-    {tags.length > 0 &&
-    <div className={styles.tags}>
-      {tags.map((tag, index) => (
-        <span className={styles.tag} key={index}>
-          {tag}
-        </span>
-      ))}
-    </div>}
+      {tags.length > 0 && (
+        <div>
+          <div className={styles.tags_title}>All tags</div>
+
+          <div className={styles.tags}>
+            {tags.map((tag, index) => (
+              <span className={styles.tag} key={index}>
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 };
